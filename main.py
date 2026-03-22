7c147116ca1c8dd578d77cf57343d86b5ef8a75b
KAMENMAIL <std.67122420327@ubru.ac.th>
2026-03-22 10:30:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import os
 from shoe_store import create_app
+from shoe_store.extensions import db
+from shoe_store.models import User  
 
 app = create_app()
 
 db_url = os.getenv("DATABASE_URL")
-
 if db_url:
-    
     if db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
     
+
     if "sslmode" not in db_url:
-        if "?" in db_url:
-            db_url += "&sslmode=require"
-        else:
-            db_url += "?sslmode=require"
+        db_url += "?sslmode=require" if "?" not in db_url else "&sslmode=require"
 
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'รหัสลับสำรองกรณีลืมตั้งในRender')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+with app.app_context():
+    try:
+        db.create_all()
+        print("Tables created successfully!")
+    except Exception as e:
+        print(f"Error creating tables: {e}")
 
 if __name__ == "__main__":
-
-  if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
