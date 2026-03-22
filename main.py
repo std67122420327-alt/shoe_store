7467282409eb2459179ab83f9de7ee3de5600f15
KAMENMAIL <std.67122420327@ubru.ac.th>
2026-03-22 12:19:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-import os
-from shoe_store import create_app
-from shoe_store.extensions import db
-from shoe_store.models import User  
+from foodapp import create_app
 
 app = create_app()
-
-db_url = os.getenv("DATABASE_URL")
-if db_url:
-    if db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
-    
-
-    if "sslmode" not in db_url:
-        db_url += "?sslmode=require" if "?" not in db_url else "&sslmode=require"
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-with app.app_context():
-    try:
-        db.create_all()
-        print("Tables created successfully!")
-    except Exception as e:
-        print(f"Error creating tables: {e}")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
